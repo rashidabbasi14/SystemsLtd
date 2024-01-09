5982ee13cd480451ea3692cb71c157336e5f8f19
Rashid Ahmed Abbasi <rashid.aabbasi@systemsltd.com>
2024-01-09 18:52:36 +0500

    Before New Changes

--- my.py
+++ my.py
@@ -2,10 +2,6 @@ from openpyxl import Workbook
 from openpyxl import load_workbook
 import os
 
-class bcolors:
-    WARNING = '\033[93m'
-    ERROR = '\033[91m'
-
 wb = load_workbook(filename = 'UserSheet.xlsx')
 nwb = Workbook()
 ws = wb.active
@@ -58,51 +54,29 @@ f5 = open("Errors/MultipleRoleError.txt", "w")
 
 for col in ws.iter_cols(min_row = minrow, min_col = mincol, max_col = maxcol, max_row = maxrow):
     for cell in col:
-        if ws['D'+str(cell.row)].value == None:
-            continue
         if ws['B'+str(cell.row)].value == None:
-            print("ERROR: Row-"+str(cell.row) + ": Name is Empty")
+            continue
+            
+        if ws['A'+str(cell.row)].value == None:
             f1.write("ERROR: Row-"+str(cell.row) + ": Name is Empty\n")
-        if len(str(ws['D'+str(cell.row)].value)) < 5:
-            print("ERROR: Row-"+str(cell.row) + ": Sign On Name String is less than 5")
+        if len(str(ws['B'+str(cell.row)].value)) < 5:
             f2.write("ERROR: Row-"+str(cell.row) + ": Sign On Name String is less than 5\n")
-        if len(str(ws['I'+str(cell.row)].value)) > 2:
-            print("WARNING Row-"+str(cell.row) + ": DAO Warning (Greater than 2)")
+        if len(str(ws['D'+str(cell.row)].value)) > 2:
             f3.write("WARNING Row-"+str(cell.row) + ": DAO Warning (Greater than 2)\n")
-        if ws['J'+str(cell.row)].value == None:
-            print("ERROR: Row-"+str(cell.row) + ": Company is Empty")
-            f4.write("ERROR: Row-"+str(cell.row) + ": Company is Empty\n")
-
-        count = 0
-        if ws['E'+str(cell.row)].value == "Y":
-            count = count + 1 
-        if ws['F'+str(cell.row)].value == "Y":
-            count = count + 1 
-        if ws['G'+str(cell.row)].value == "Y":
-            count = count + 1 
-        if ws['H'+str(cell.row)].value == "Y":
-            count = count + 1
-        if count > 1: 
-            print("ERROR: Row-"+str(cell.row) + ": Multiple roles are assigned")
-            f5.write("ERROR: Row-"+str(cell.row) + ": Multiple roles are assigned\n")
-            
-        if ws['E'+str(cell.row)].value == None or ws['F'+str(cell.row)].value == None or ws['G'+str(cell.row)].value == None or ws['H'+str(cell.row)].value == None:
-            print("ERROR: Row-"+str(cell.row) + ": Empty Field")
-            f5.write("ERROR: Row-"+str(cell.row) + ": Empty Field\n")
-            
-        if ws['E'+str(cell.row)].value == None and ws['F'+str(cell.row)].value == None and ws['G'+str(cell.row)].value == None and ws['H'+str(cell.row)].value == None:
-            print("ERROR: Row-"+str(cell.row) + ": ALL Role Empty Field")
-            f5.write("ERROR: Row-"+str(cell.row) + ": ALL Role Empty Field\n")
+        if ws['E'+str(cell.row)].value == None or len(str(ws['E'+str(cell.row)].value)) != 9:
+            f4.write("ERROR: Row-"+str(cell.row) + ": Primary Company is Empty or not length of 9\n")
+        if ws['C'+str(cell.row)].value == None:
+            f5.write("ERROR: Row-"+str(cell.row) + ": Role is Field\n")
 
         nws['A'+str(cell.row)] = ""
-        nws['B'+str(cell.row)] = "U."+str(ws['D'+str(cell.row)].value).rjust(5,"0") if ws['D'+str(cell.row)].value else ""
-        nws['C'+str(cell.row)] = ws['B'+str(cell.row)].value.upper().strip() if ws['B'+str(cell.row)].value else ""
-        nws['D'+str(cell.row)] = str(ws['D'+str(cell.row)].value).rjust(5,"0") if ws['D'+str(cell.row)].value else ""
+        nws['B'+str(cell.row)] = "U."+str(ws['B'+str(cell.row)].value).rjust(5,"0") if ws['B'+str(cell.row)].value else ""
+        nws['C'+str(cell.row)] = str(ws['A'+str(cell.row)].value).upper().strip() if ws['A'+str(cell.row)].value else ""
+        nws['D'+str(cell.row)] = str(ws['B'+str(cell.row)].value).rjust(5,"0") if ws['B'+str(cell.row)].value else ""
         nws['E'+str(cell.row)] = "INT"
         nws['F'+str(cell.row)] = "1"
-        nws['H'+str(cell.row)] = ws['I'+str(cell.row)].value
+        nws['H'+str(cell.row)] = ws['D'+str(cell.row)].value
         nws['I'+str(cell.row)] = "20240101M0101"
-        nws['J'+str(cell.row)] = "20231412"
+        nws['J'+str(cell.row)] = "20231218"
         nws['K'+str(cell.row)] = "20990321"
         nws['L'+str(cell.row)] = "00:00"
         nws['M'+str(cell.row)] = "24:00"
@@ -117,65 +91,90 @@ for col in ws.iter_cols(min_row = minrow, min_col = mincol, max_col = maxcol, ma
         nws['Y'+str(cell.row)] = "00"
         nws['Z'+str(cell.row)] = "?."
         nws['AA'+str(cell.row)] = "1"
-
-        if(ws['J'+str(cell.row)].value):
-            appl = ""
-            func = ""
-            if ws['E'+str(cell.row)].value == "Y": #Maker
-                appl = "ALL.PG"
-                func = "B C D E F H I L P R S V"
-            elif ws['F'+str(cell.row)].value == "Y": #Checker
-                appl = "@ASA.PK.A.CAD"
-                func = ""
-            elif ws['G'+str(cell.row)].value == "Y": #View Only
-                appl = "ALL.PG"
-                func = "H L P S V"
-            elif ws['H'+str(cell.row)].value == "Y": #All Rights
-                appl = "ALL.PG"
-                func = "A 2 B C D E F H I L P R S V"
-            
-            COMPANY = ws['J'+str(cell.row)].value.replace(" ","")
+               
+        appl = "ALL.PG" #Other than checker
+        c_appl = "@ASA.PK.A.CAD" #Checker
+        m_func = "B C D E F H I L P R S V" #Maker
+        c_func = "" #Checker
+        v_func = "H L P S V" #View Only
+        a_func = "A 2 B C D E F H I L P R S V" #All Rights
+        
+        P_COMPANY = ws['E'+str(cell.row)].value.replace(" ","")
+        P_COMPANY = P_COMPANY.replace(",","")
+        P_COMPANY = "ALL::"+P_COMPANY
+        
+        if ws['F'+str(cell.row)].value != None:
+            COMPANY = ws['F'+str(cell.row)].value.replace(" ","")
             COMPANY = COMPANY.replace(",,",",")
             COMPANY = COMPANY.replace(",","::")
-            COMPANY = "ALL::"+COMPANY
-            items = COMPANY.split("::")
-            items = [x for n, x in enumerate(items) if x not in items[:n]]
-            items = [x for n, x in enumerate(items) if len(x) == 9 or x == "ALL"]
-            
-            COMPANY = "::".join(items)
-            
-            xlen = len(items)
-            application = appl
-            function = func
-            for x in range(xlen-1):
-                if appl:
-                    application += "::"+appl
-                if func:
-                    function += "::"+func
-
-            if xlen > 2:
-                COMPANY = COMPANY.replace("ALL::","")
-                
-            nws['G'+str(cell.row)] = COMPANY
-            
-            if xlen > 2:
-                nws['P'+str(cell.row)] = COMPANY + "::ALL"
+            COMPANY = P_COMPANY+"::"+COMPANY
+        else:
+            COMPANY = P_COMPANY
+        
+        items = COMPANY.split("::")
+        items = [x for n, x in enumerate(items) if x not in items[:n]]
+        items = [x for n, x in enumerate(items) if len(x) == 9 or x == "ALL"]
+        
+        COMPANY = "::".join(items)
+        
+        xlen = len(items)
+        application = ""
+        function = ""
+        for x in range(xlen):
+            if x < 2:
+                if ws['C'+str(cell.row)].value == "Maker":
+                    application += appl+"::"
+                    function    += m_func+"::"
+                elif ws['C'+str(cell.row)].value == "Checker":
+                    application += c_appl+"::"
+                    function    += c_func+"::"
+                elif ws['C'+str(cell.row)].value == "View":
+                    application += appl+"::"
+                    function    += v_func+"::"
+                elif ws['C'+str(cell.row)].value == "ALL":
+                    application += appl+"::"
+                    function    += a_func+"::"
             else:
-                nws['P'+str(cell.row)] = COMPANY
+                application +=  appl+"::"
+                function += v_func+"::"
                 
-            nws['Q'+str(cell.row)] = application
-            nws['R'+str(cell.row)] = function
+        application = application[0:len(application)-2]
+        function = function[0:len(function)-2]
+            
+        if xlen > 2:
+            COMPANY = COMPANY.replace("ALL::","")
+            
+        nws['G'+str(cell.row)] = COMPANY
+        
+        if xlen > 2:
+            nws['P'+str(cell.row)] = COMPANY + "::ALL"
+            
+            t_application = application.split("::")
+            t_function = function.split("::")
+            
+            t_application = t_application[1:] + t_application[:1]
+            t_function = t_function[1:] + t_function[:1]
+            
+            application = "::".join(t_application)
+            function = "::".join(function)
+        else:
+            nws['P'+str(cell.row)] = COMPANY
+            
+        nws['Q'+str(cell.row)] = application
+        nws['R'+str(cell.row)] = function
             
 nwb.save('DMT.xlsx')
+
+
+
+
 
 f1.close()
 f2.close()
 f3.close()
 f4.close()
 f5.close()
-
 files = ["Errors/nameError.txt","Errors/signOnNameError.txt","Errors/DAO Warning.txt","Errors/CompanyNameEmpty.txt","Errors/MultipleRoleError.txt"]
-
 for file in files:
     file_stats = os.stat(file)
     if(file_stats.st_size == 0):
